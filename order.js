42756fff22b5fd0548b8af873d71b8b01a668464
affluent jacob <jacobserverside@gmail.com>
2022-09-23 07:10:28 +0100

    Admin Dashboard with HTML, CSS and Javascript

---
+++ order.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Wearable AI System from Allurine',
+        productNumber: '85631',
+        paymentStatus: 'refunded',
+        shipping: 'declined'
+    },
+    {
+        productName: 'Software for Integrated development',
+        productNumber: '85631',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'M4 Machine learning System',
+        productNumber: '85631',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    }
+]
